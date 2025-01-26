8d6396c55505bd94e93158fb9789340359c46cce
Supannee Tanathong <stanathong@yahoo.co.uk>
2025-01-27 02:07:22 +0700

    Add code for Section E.4

---
+++ appE/code/LoRALayer.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import math
+
+class LoRALayer(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        self.A = torch.nn.Parameter(torch.empty(in_dim, rank)) # skip initialisation with empty
+        torch.nn.init.kaiming_uniform_(self.A, a=math.sqrt(5)) # initialise inplace
+        self.B = torch.nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
